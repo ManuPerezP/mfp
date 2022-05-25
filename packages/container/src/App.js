4a143dec91f5993a54007fa9929bfc642d0ccf7e
Manuel Pérez <manuel.perez.pozo@gmail.com>
2022-05-24 20:19:49 -0400

    add invalidation to refresh las main build file

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import MarketingApp from './components/MarketingApp'
 
 export default ()=>{
-    return <><h1>Hi there! from app container</h1>
+    return <><h1>Hi there! from app container latest</h1>
     <hr/>
     <MarketingApp/></>
 }
