0341344575b77f46186d95744fc13d951bc94916
Manuel Pérez <manuel.perez.pozo@gmail.com>
2022-05-24 22:50:06 -0400

    add header and router

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,8 +1,15 @@
-import React from 'react'
-import MarketingApp from './components/MarketingApp'
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
 
-export default ()=>{
-    return <><h1>Hi there! from app container latest</h1>
-    <hr/>
-    <MarketingApp/></>
-}
+export default () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
+  );
+};
